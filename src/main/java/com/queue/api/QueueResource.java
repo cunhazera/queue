779a8d751a8a3be3094d810a6f0837acb15c6462
cunhazera <cunha.gabriel919@gmail.com>
2017-07-02 15:47:47 -0300

    Changing end point class

--- src/main/java/com/queue/api/QueueResource.java
+++ src/main/java/com/queue/api/QueueResource.java
@@ -1,18 +1,44 @@
 package com.queue.api;
 
-import com.queue.domain.LinkedQueue;
+import com.queue.domain.ArrayQueue;
 import com.queue.domain.Queue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class QueueResource {
 
-    private Queue<Integer> queue = new LinkedQueue<>();
+    private static Queue<Integer> queue;
 
-    @RequestMapping("/queue")
-    public Queue<Integer> queue() {
-        return queue;
+    @RequestMapping("/init")
+    public Collection<Integer> initialize(@RequestParam("size") Integer size) {
+        queue = new ArrayQueue<>(size);
+        return queue.values();
+    }
+
+    @RequestMapping("/enqueue")
+    public Collection<Integer> enqueue(@RequestParam("num") Integer num) {
+        queue.enqueue(num);
+        return queue.values();
+    }
+
+    @RequestMapping("/dequeue")
+    public Collection<Integer> dequeue() {
+        queue.dequeue();
+        return queue.values();
+    }
+
+    @RequestMapping("/front")
+    public Integer front() {
+        return queue.front();
+    }
+
+    @RequestMapping("/values")
+    public Collection<Integer> values() {
+        return queue.values();
     }
 
 }
--- src/main/java/com/queue/app/AppApplication.java
+++ src/main/java/com/queue/app/AppApplication.java
@@ -1,51 +1,13 @@
 package com.queue.app;
 
-import com.queue.domain.ArrayQueue;
-import com.queue.domain.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
-
-@SpringBootApplication
-@RestController
+@SpringBootApplication(scanBasePackages = {"com.queue"})
 public class AppApplication {
-
-    private static Queue<Integer> queue;
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
-    }
-
-    @RequestMapping("/init")
-    public Collection<Integer> initialize(@RequestParam("size") Integer size) {
-        queue = new ArrayQueue<>(size);
-        return queue.values();
-    }
-
-    @RequestMapping("/enqueue")
-    public Collection<Integer> enqueue(@RequestParam("num") Integer num) {
-        queue.enqueue(num);
-        return queue.values();
-    }
-
-    @RequestMapping("/dequeue")
-    public Collection<Integer> dequeue() {
-        queue.dequeue();
-        return queue.values();
-    }
-
-    @RequestMapping("/front")
-    public Integer front() {
-        return queue.front();
-    }
-
-    @RequestMapping("/values")
-    public Collection<Integer> values() {
-        return queue.values();
     }
 
 }
